aabc1dbfdb7afbf9302d62b6ef26d33600bddf03
Lucas Baizer <titaniumsapphire32@gmail.com>
2017-08-05 14:29:54 -0700

    finish with buttons

--- src/IntroToArrayLists/GuestBook.java
+++ src/IntroToArrayLists/GuestBook.java
@@ -1,6 +1,30 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
 public class GuestBook {
+	public static void main(String[] args) {
+		GuestBook guestbook = new GuestBook();
+		guestbook.createUI();
+	}
+	JFrame newframe = new JFrame("Guest Book");
+	JPanel newpanel = new JPanel();
+	JButton addname = new JButton("Add Name");
+	JButton viewnames = new JButton("View Names");
+	public void createUI() {
+		newframe.setVisible(true);
+		newframe.add(newpanel);
+		newpanel.add(addname);
+		newpanel.add(viewnames);
+		if (addname) {
+			JOptionPane.showInputDialog("Enter a Name");
+		}
+	}
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
--- src/IntroToArrayLists/IntroToArrayLists.java
+++ src/IntroToArrayLists/IntroToArrayLists.java
@@ -1,20 +1,40 @@
 package IntroToArrayLists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
+		ArrayList<String> array = new ArrayList<>();
 		//   Don't forget to import the ArrayList class
 		
 		//2. Add five Strings to your list
-		
+		array.add("String one");
+		array.add("String two");
+		array.add("String three");
+		array.add("String four");
+		array.add("String five");
 		//3. Print all the Strings using a standard for-loop
+	
 		
+	//	for (String e:array) {
+		//	System.out.println(e);
+		//}
 		//4. Print all the Strings using a for-each loop
 		
 		//5. Print only the even numbered elements in the list.
-		
+		//for (int i = 0; i < array.size(); i+=2) {
+		//	System.out.println(i);
+		//}
 		//6. Print all the Strings in reverse order.
-		
+		//for (int i = 4; i >=0; i--) {
+		//	System.out.println(i);
+		//}
 		//7. Print only the Strings that have the letter 'e' in them.
+		for (int i = 0; i < array.size(); i++){
+			if(array.get(i).contains("e"));{
+			System.out.println(i);
+			}
+		}
 	}
 }
